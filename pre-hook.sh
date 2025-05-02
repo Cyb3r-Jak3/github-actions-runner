80d268fcbab5810973681782773f0e50a37295b1
Cyb3r-Jak3 <git@Cyberjake.xyz>
2025-05-02 18:41:21 -0400

    set -xe is too verbose

--- pre-hook.sh
+++ pre-hook.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
-set -xe
 source $HOME/.nvm/nvm.sh
 
 {
-    echo PATH=$PATH:$HOME/.local/bin:$HOME/.tofuenv/bin
+    echo PATH="$PATH":"$HOME"/.local/bin:"$HOME"/.tofuenv/bin
     echo TOFUENV_ROOT="$HOME/.tofuenv"
-    echo NVM_DIR="$HOME/.nvm "
+    echo NVM_DIR="$HOME/.nvm"
 } >> "$GITHUB_ENV"

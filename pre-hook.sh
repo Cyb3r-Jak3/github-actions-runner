62f617cc0b97c305fb57f9ff95ead68fd2fc5441
Cyb3r-Jak3 <git@Cyberjake.xyz>
2025-07-08 18:13:16 -0400

    Add more software to be pre-installed

--- pre-hook.sh
+++ pre-hook.sh
@@ -1,8 +1,2 @@
 #!/bin/bash
-source $HOME/.nvm/nvm.sh
 
-{
-    echo PATH="$PATH":"$HOME"/.local/bin:"$HOME"/.tofuenv/bin
-    echo TOFUENV_ROOT="$HOME/.tofuenv"
-    echo NVM_DIR="$HOME/.nvm"
-} >> "$GITHUB_ENV"
